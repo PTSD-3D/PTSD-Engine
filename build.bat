6a49e77f97c7ae366a3da08b4b44e520cb49c78e
Ricardo <48621751+Drathijin@users.noreply.github.com>
2021-03-12 11:33:32 +0100

    Script básico para mover los .dll de ogre a bin (#1)

---
+++ build.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+:: Por ahora voy a hacer esto para mover los .dll de Ogre a nuestro /bin/ Pero en teoría
+::además debería de buildear Ogre llamando a cmake con parametros y demás
+TITLE Build
+
+echo Copiando DLLs...
+
+copy dependencies\Ogre\build\bin\debug\OgreMain_d.dll bin\ 1>NUL
+copy dependencies\Ogre\build\bin\relwithdebinfo\OgreMain.dll bin\ 1>NUL
+copy dependencies\Ogre\build\bin\relwithdebinfo\zlib.dll bin\ 1>NUL
+
+PAUSE
