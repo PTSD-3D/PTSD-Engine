687e05e85c1698e1c071810333104d34238bf30c
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-03-19 12:45:03 +0100

    Primeros pasitos
    Por ahora Ogre root no sabe nada de quien es sdl, parece.
    Si hay que hacer include de SDL como tal el .c esta en: SDL.c esta en PTSD-Engine\dependencies\Ogre\build\SDL2-2.0.10\src\SDL.c

--- projects/PTSD-Core/src/test.cpp
+++ projects/PTSD-Core/src/test.cpp
@@ -12,6 +12,25 @@ namespace PTSD
 		Ogre::Root* root;
 		root = new Ogre::Root();
 
+		//SDL.c esta en PTSD-Engine\dependencies\Ogre\build\SDL2-2.0.10\src\SDL.c
+
+		/*SDL_Init(SDL_INIT_EVERYTHING);
+
+		SDL_Surface* screen = SDL_SetVideoMode(640, 480, 8, SDL_SWSURFACE);;
+
+		Ogre::RenderWindow* window;
+		root->initialise(false);
+
+		SDL_SysWMinfo wmi;
+		SDL_VERSION(&wmi.version);
+
+		SDL_GetWMInfo(&wmi);
+
+
+		Ogre::NameValuePairList misc;
+		misc["externalWindowHandle"] = Ogre::StringConverter::toString((unsigned int)wmi.window);
+		window = root->createRenderWindow("Render Window", 800, 600, false, &misc);*/
+
 		std::cout << "Hola Ogre\n";
 	}
 }
