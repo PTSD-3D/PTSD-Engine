efdbaa6ddcdea2757c08916b5e5707b586f9edac
AdriAlvarez00 <adralv01@ucm.es>
2021-03-20 11:47:18 +0100

    Creado el sistema de Log

--- projects/PTSD-Core/src/main.cpp
+++ projects/PTSD-Core/src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "test.h"
+#include "PTSDlog.h"
 
 int main()
 {
 	PTSD::test();
+	PTSD::Log* m_LogSystem = new PTSD::Log();
+	m_LogSystem->Init();
 }
---
+++ projects/PTSD-Logger/src/PTSDLog.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "PTSDLog.h"
+
+namespace PTSD {
+	int Log::Init() {
+		std::cout << "Sistema de logging inicializado\n";
+		
+		return 0;
+	}
+}
