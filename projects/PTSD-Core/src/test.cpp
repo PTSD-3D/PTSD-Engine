1295c8fc9ec4fbe8fcb6c1cbd0db530a8e2603a0
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-03-20 19:11:08 +0100

    Pruebas incluyendo archivos de SDL
    He probado a incluir los archvos de sdl qie estan en la build de Ogre, tando su codigo fuente como el buildeado.
    
    Tambien he probado a pedirle a Ogre que me cree él una ventana, pero creo que lo he hecho mal

--- projects/PTSD-Core/src/test.cpp
+++ projects/PTSD-Core/src/test.cpp
@@ -1,6 +1,7 @@
 #include "test.h"
 #include <iostream>
 #include <OgreRoot.h>
+//#include <SDL.c>
 
 
 namespace PTSD
@@ -12,24 +13,26 @@ namespace PTSD
 		Ogre::Root* root;
 		root = new Ogre::Root();
 
+		root->createRenderWindow("hola", 400, 300, false);
+
 		//SDL.c esta en PTSD-Engine\dependencies\Ogre\build\SDL2-2.0.10\src\SDL.c
 
-		/*SDL_Init(SDL_INIT_EVERYTHING);
+		//SDL_Init(SDL_INIT_EVERYTHING);
 
-		SDL_Surface* screen = SDL_SetVideoMode(640, 480, 8, SDL_SWSURFACE);;
+		//SDL_Surface* screen = SDL_SetVideoMode(640, 480, 8, SDL_SWSURFACE);;
 
-		Ogre::RenderWindow* window;
-		root->initialise(false);
+		//Ogre::RenderWindow* window;
+		//root->initialise(false);
 
-		SDL_SysWMinfo wmi;
-		SDL_VERSION(&wmi.version);
+		//SDL_SysWMinfo wmi;
+		//SDL_VERSION(&wmi.version);
 
-		SDL_GetWMInfo(&wmi);
+		//SDL_GetWMInfo(&wmi);
 
 
-		Ogre::NameValuePairList misc;
-		misc["externalWindowHandle"] = Ogre::StringConverter::toString((unsigned int)wmi.window);
-		window = root->createRenderWindow("Render Window", 800, 600, false, &misc);*/
+		//Ogre::NameValuePairList misc;
+		//misc["externalWindowHandle"] = Ogre::StringConverter::toString((unsigned int)wmi.window);
+		//window = root->createRenderWindow("Render Window", 800, 600, false, &misc);
 
 		std::cout << "Hola Ogre\n";
 	}
