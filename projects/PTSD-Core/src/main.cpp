aa23836308e7c6c2d31ef1eff579ce62d6e54022
Will7461 <43720307+Will7461@users.noreply.github.com>
2021-03-22 23:04:13 +0100

    merge con main

--- projects/PTSD-Core/src/main.cpp
+++ projects/PTSD-Core/src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "test.h"
 #include "PTSDLog.h"
+#include "PTSDUI.h"
 
 int main()
 {
 	PTSD::test();
 	PTSD::Log* m_LogSystem = new PTSD::Log();
 	m_LogSystem->Init();
+	
+	PTSD::UI* m_UISystem = new PTSD::UI();
+	m_UISystem->Init();
 }
