5a09351ba5820fa5e21d35cf2d3310121fe4d0dc
Adrian-de-Lucas-Gomez <47756657+Adrian-de-Lucas-Gomez@users.noreply.github.com>
2021-03-23 17:28:31 +0100

    Proyecto de input creado pero sin finalizar
    Ya lo tenemos configurado pero aun no deja compilar al core. Por si solo si genera las .dll y librerias y las pone en el bin
    
    Co-Authored-By: evalucas <48751995+evalucas@users.noreply.github.com>

--- projects/PTSD-Core/src/main.cpp
+++ projects/PTSD-Core/src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "test.h"
 
+#include "PTSDInput.h"
+
 int main()
 {
 	PTSD::test();
+
+	PTSD::Input* m_InputSystem = new PTSD::Input();
+	m_InputSystem->Init();
 }
--- projects/PTSD-Core/src/test.cpp
+++ projects/PTSD-Core/src/test.cpp
@@ -16,57 +16,6 @@ namespace PTSD
 		Ogre::Root* root;
 		root = new Ogre::Root();
 
-
-		SDL_Init(SDL_INIT_EVERYTHING);
-
-		if (!SDL_WasInit(SDL_INIT_VIDEO))
-			SDL_InitSubSystem(SDL_INIT_VIDEO);
-
-		Uint32 flags = SDL_WINDOW_ALLOW_HIGHDPI; //SDL_WINDOW_RESIZABLE
-
-		SDL_Window* sdlWindow = SDL_CreateWindow("HOLI", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 300, flags);
-
-		SDL_SysWMinfo wmInfo;
-		SDL_VERSION(&wmInfo.version);
-		if (SDL_GetWindowWMInfo(sdlWindow, &wmInfo) == SDL_FALSE) {
-			OGRE_EXCEPT(Ogre::Exception::ERR_INTERNAL_ERROR,
-				"Couldn't get WM Info! (SDL2)",
-				"BaseApplication::setup");
-		}
-
-		Ogre::NameValuePairList params;
-
-		/*params["FSAA"] = CurrentGraphicsConfiguration["FSAA"].currentValue;
-		params["vsync"] = false;
-		params["gamma"] = false;
-
-		params["externalWindowHandle"] = Ogre::StringConverter::toString(size_t(wmInfo.info.win.window));*/
-
-		//root->createRenderWindow("HOLI", 200, 200, false);
-
-
-		//////////por si queremos que la ventana oculte el cursor
-		SDL_SetWindowGrab(sdlWindow, SDL_bool(false));
-		SDL_ShowCursor(false);
-
-		SDL_Event e;
-
-		bool paco = false;
-
-		while (!paco) {
-
-			SDL_PollEvent(&e);
-			if (e.type == SDL_KEYDOWN) {
-				switch (e.key.keysym.sym) {
-				case SDLK_LEFT:
-					std::cout << "Wenas tardes" << "\n";
-					paco = true;
-					break;
-				}
-			}
-			
-		}
-
 		std::cout << "Hola Ogre\n";
 	}
 }
