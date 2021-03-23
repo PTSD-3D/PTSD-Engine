017008b79945ce535d0bfc86bfac73268edd9cf1
AdriAlvarez00 <adralv01@ucm.es>
2021-03-23 10:40:43 +0100

    .bat automatiza copias de .dll de CEGUI

--- build.bat
+++ build.bat
@@ -3,10 +3,17 @@
 ::además debería de buildear Ogre llamando a cmake con parametros y demás
 TITLE Build
 
-echo Copiando DLLs...
+echo Copiando DLLs de Ogre...
 
 copy dependencies\Ogre\build\bin\debug\OgreMain_d.dll bin\ 1>NUL
 copy dependencies\Ogre\build\bin\relwithdebinfo\OgreMain.dll bin\ 1>NUL
 copy dependencies\Ogre\build\bin\relwithdebinfo\zlib.dll bin\ 1>NUL
+
+echo Copiando DLLs de CEGUI...
+copy dependencies\CEGUI\build\bin\CEGUIBase-0_d.dll bin\ 1>NUL
+copy dependencies\CEGUI\src\dependencies\bin\freetype.dll bin\ 1>NUL
+copy dependencies\CEGUI\src\dependencies\bin\freetype_d.dll bin\ 1>NUL
+copy dependencies\CEGUI\src\dependencies\bin\pcre.dll bin\ 1>NUL
+copy dependencies\CEGUI\src\dependencies\bin\pcre_d.dll bin\ 1>NUL
 
 PAUSE
