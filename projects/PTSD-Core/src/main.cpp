0c0e9908ea15c095aafed7392f7b5bd9e25f6a2d
Miguel Mur Cortés <mmur@ucm.es>
2021-03-29 19:10:16 +0200

    Fmod funcional en PTSD-Sound (#15)
    * Creado el proyecto PTSD-Sound. Procedo a intentar a enganchar fmod
    
    * Fmod funcional!
    
    * Musiquita desbloqueada :)
    
    * Arreglada la ruta
    
    * Subido el .tar para que ricardo haga magia de linux
    
    * magia de linux sin cmake :D
    
    * Añadidas las instrucciones y cambios de nombres de métodos en PTSD-Sound y PTSD-Physics (Para que todas las pruebas sean con Init)
    
    * No termine de rellenar las instrucciones
    
    * Arreglada la ruta
    
    * No se por que visual no petaba con esto
    
    Co-authored-by: Drathijin <rsulbara@ucm.es>

--- projects/PTSD-Core/src/main.cpp
+++ projects/PTSD-Core/src/main.cpp
@@ -4,6 +4,7 @@
 #include "PTSDPhysics.h"
 #include "PTSDGraphics.h"
 #include "PTSDScripting.h"
+#include "PTSDSound.h"
 #include "PTSDUI.h"
 
 #include "PTSDInput.h"
@@ -14,14 +15,17 @@ int main()
 	PTSD::Input* m_InputSystem = new PTSD::Input();
 	PTSD::Graphics* m_GraphicsSystem = new PTSD::Graphics();
 	PTSD::Scripting* m_ScriptingSystem = new PTSD::Scripting();
+	PTSD::PTSDPhysics* m_physicsSystem = new PTSD::PTSDPhysics();
 	PTSD::UI* m_UISystem = new PTSD::UI();
+	PTSD::PTSDSound* m_soundSystem = new PTSD::PTSDSound();
 	PTSD::PTSDPhysics* physicsSystem = new PTSD::PTSDPhysics();
 
 	
 	m_LogSystem->Init(PTSD::Info);
 	PTSD::LOG("Beginning Initialization");
+	m_soundSystem->Init();
 	m_InputSystem->Init();
-	physicsSystem->test();
+	physicsSystem->Init();
 	m_GraphicsSystem->Init();
 	m_ScriptingSystem->Init();
 	PTSD::LOG("All subsystems initialized");
---
+++ projects/PTSD-Sound/include/PTSDSound.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifdef _PTSDSOUND
+#define SOUNDAPI  __declspec(dllexport)   // export DLL information
+
+#else
+#define SOUNDAPI  __declspec(dllimport)   // import DLL information
+
+#endif 
+
+namespace PTSD {
+	class PTSDSound
+	{
+	public:
+		PTSDSound() {};
+		int SOUNDAPI Init();
+	};
+}
