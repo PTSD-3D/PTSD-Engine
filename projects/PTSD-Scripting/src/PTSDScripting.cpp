72aee83cfe4c07677c74c32ee634eeb875f849ee
Esteban <35503563+EstebenR@users.noreply.github.com>
2021-04-01 13:27:45 +0200

    Interfaz scripting (#21)
    Co-Authored-By: Ricardo <48621751+Drathijin@users.noreply.github.com>
    
    Co-authored-by: Ricardo <48621751+Drathijin@users.noreply.github.com>

--- projects/PTSD-Scripting/src/PTSDScripting.cpp
+++ projects/PTSD-Scripting/src/PTSDScripting.cpp
@@ -1,15 +1,55 @@
 #include <iostream>
 #include "PTSDScripting.h"
+#include "ScriptingImplementation.h"
 #include "lua.hpp"
 #include <sol/sol.hpp>
 
+
 namespace PTSD {
-	void Scripting::init() {
+	int Scripting::init() {
 		std::cout << "Initializing lua scripting system\n";
 		
-		sol::state lua;
-		lua.open_libraries(sol::lib::base);
+		sol::state luaState;
+		luaState.open_libraries(sol::lib::base);
 
-		lua.script("print('Lua initialized correctly and working!')");
+		mScriptMgr = new ScriptingImplementation();
+		if (!mScriptMgr->init()) {
+			printf("F");
+		}
+		else {
+			luaState.script("print('Lua initialized correctly and working!')");
+		}
+
+		return 0;
+	}
+
+	Scripting::~Scripting() {
+		if (mScriptMgr != nullptr)
+			delete mScriptMgr;
+	}
+
+	void Scripting::run(std::string script)
+	{
+		mScriptMgr->run(script);
+	}
+
+	bool Scripting::update()
+	{
+		return mScriptMgr->update();
+	}
+
+	void Scripting::shutdown()
+	{
+		mScriptMgr->shutdown();
+	}
+
+	void Scripting::addEntity(void* entityPtr)
+	{
+		mScriptMgr->addEntity(entityPtr);
+	}
+
+	void Scripting::deleteEntity(size_t entityID)
+	{
+		mScriptMgr->deleteEntity(entityID);
 	}
 }
