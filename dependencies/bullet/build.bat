36a11263af98c57f1a7fe24270ac75156088e4c1
AdriAlvarez00 <adralv01@ucm.es>
2021-04-25 19:16:31 +0200

    Arreglado gitignore y batch files

---
+++ dependencies/bullet/build.bat
@@ -0,0 +1,16 @@
+:: Build Bullet
+if exist \src\build3\vs2010 goto done
+
+cd src
+call build_visual_studio_vr_pybullet_double_dynamic_PTSD.bat
+cd vs2010
+
+MSBuild 0_Bullet3Solution.sln /t:pybullet /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=v142
+MSBuild 0_Bullet3Solution.sln /t:pybullet /p:Configuration=Debug /p:Platform=x64 /p:PlatformToolset=v142
+
+cd ..\..\..\
+goto end
+
+:done
+echo Bullet already built
+:end
