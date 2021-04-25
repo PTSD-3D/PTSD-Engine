729d3a375da3734b287c1fb431245c1d2c45d98b
Esteban <35503563+EstebenR@users.noreply.github.com>
2021-04-25 10:32:38 +0200

    Actualizado para que vaya en windows

--- source/Scripting/include/MeshComponent.h
+++ source/Scripting/include/MeshComponent.h
@@ -1,20 +1,21 @@
 #include "Component.h"
 #include "string.h"
 
-namespace Ogre{class Entity;class SceneNode;}
-namespace PTSD{
-	class MeshComponent: public Component
+namespace Ogre { class Entity; class SceneNode; }
+
+namespace PTSD {
+	class MeshComponent : public Component
 	{
 	public:
 		MeshComponent();
 		MeshComponent(const std::string&);
-		MeshComponent(const std::string&,const std::string&);
+		MeshComponent(const std::string&, const std::string&);
 		virtual void init() override;
 		void setMesh(const std::string& mesh);
 		void setMaterial(const std::string& material);
-		const std::string getMesh(){return mMesh_;};
-		const std::string getMaterial(){return mMaterial_;};
-	
+		const std::string getMesh() { return mMesh_; };
+		const std::string getMaterial() { return mMaterial_; };
+
 	private:
 		Ogre::Entity* getEntity(); //helper function
 		Ogre::SceneNode* sceneNode = nullptr;
