8db3ff6a559b8ebe0fd169082bc762d563a644be
AdriAlvarez00 <adralv01@ucm.es>
2021-04-25 18:47:44 +0200

    Inicio automatización

--- build.bat
+++ build.bat
@@ -3,6 +3,17 @@
 ::además debería de buildear Ogre llamando a cmake con parametros y demás
 TITLE Build
 
+echo "Example: for C:\User\Things\Python39 introduce C:\User\Things"
+set /p inPy="Introduce the folder where PythonX.X is stored:"
+
+cd dependencies\Ogre\
+call build.bat
+cd ..\CEGUI\
+call build.bat
+cd ..\bullet\
+call build.bat
+cd ..\..\
+
 echo Copiando DLLs de Ogre...
 
 copy dependencies\Ogre\build\bin\debug\OgreMain_d.dll bin\ 1>NUL
@@ -13,21 +24,22 @@ copy dependencies\Ogre\build\bin\release\RenderSystem_GL.dll bin\ 1>NUL
 copy dependencies\Ogre\build\bin\release\Codec_STBI.dll bin\ 1>NUL
 copy dependencies\Ogre\build\bin\debug\Codec_STBI_d.dll bin\ 1>NUL
 
-echo Copiando DLLs de Bullet...
-copy dependencies\bullet\src\bin\pybullet_vs2010_x64_release.dll bin\ 1>NUL
-copy dependencies\bullet\src\bin\pybullet_vs2010_x64_debug.dll bin\ 1>NUL
-
 echo Copiando DLLs de SDL2...
+
 copy dependencies\Ogre\build\SDL2-build\Debug\SDL2d.dll bin\ 1>NUL
 copy dependencies\Ogre\build\SDL2-build\RelWithDebInfo\SDL2.dll bin\ 1>NUL
-copy dependencies\fmod\fmod.dll bin\ 1>NUL
-copy dependencies\fmod\fmodL.dll bin\ 1>NUL
+
+echo Copiando DLLs de Bullet...
+
+copy dependencies\bullet\src\bin\pybullet_vs2010_x64_release.dll bin\ 1>NUL
+copy dependencies\bullet\src\bin\pybullet_vs2010_x64_debug.dll bin\ 1>NUL
 
 echo Copiando DLLs de FMOD...
 copy dependencies\fmod\fmod.dll bin\ 1>NUL
 copy dependencies\fmod\fmodL.dll bin\ 1>NUL
 
 echo Copiando DLLs de CEGUI...
+
 copy dependencies\CEGUI\build\bin\CEGUIBase-0_d.dll bin\ 1>NUL
 copy dependencies\CEGUI\build\bin\CEGUIBase-0.dll bin\ 1>NUL
 copy dependencies\CEGUI\build\bin\CEGUIOgreRenderer-0.dll bin\ 1>NUL
@@ -38,8 +50,6 @@ copy dependencies\CEGUI\src\dependencies\bin\libexpat.dll bin\ 1>NUL
 copy dependencies\CEGUI\src\dependencies\bin\libexpat_d.dll bin\ 1>NUL
 copy dependencies\CEGUI\src\dependencies\bin\pcre.dll bin\ 1>NUL
 copy dependencies\CEGUI\src\dependencies\bin\pcre_d.dll bin\ 1>NUL
-copy dependencies\Ogre\build\bin\release\Codec_STBI.dll bin\ 1>NUL
-copy dependencies\Ogre\build\bin\debug\Codec_STBI_d.dll bin\ 1>NUL
 copy dependencies\CEGUI\build\bin\CEGUIOgreRenderer-0.dll bin\ 1>NUL
 copy dependencies\CEGUI\build\bin\CEGUIOgreRenderer-0_d.dll bin\ 1>NUL
 copy dependencies\CEGUI\src\dependencies\bin\libexpat.dll bin\ 1>NUL
