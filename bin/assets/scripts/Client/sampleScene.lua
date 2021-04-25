1109c87b8383cfb2d0accecdd3c426cc94a8ed03
Drathijin <rsulbara@ucm.es>
2021-04-25 15:05:19 +0200

    añadiendo algo de documentación

--- bin/assets/scripts/Client/sampleScene.lua
+++ bin/assets/scripts/Client/sampleScene.lua
@@ -8,14 +8,14 @@ Scene = {
 			{name="playerMove", arguments={1,1,1}}
 		},
 		Transform = {position={x=0,y=0,z=-250},rotation={x=0,y=0,z=0},scale={x=1,y=1,z=1}},
-		Mesh = {mesh="ogrehead.mesh", material="Sinbad/Body"}
+		Mesh = {mesh="ogrehead.mesh", material="KirbyMat"}
 	},
 	Entity2={
 		Components = {
 			{name="playerMove", arguments={1,1,1}}
 		},
 		Transform = {position={x=-0,y=30,z=-100},rotation={x=-90,y=0,z=0},scale={x=1,y=1,z=1}},
-		Mesh = {mesh="Kirby.mesh", material="Sinbad/Body"}
+		Mesh = {mesh="Kirby.mesh", material="KirbyMat"}
 	}
 }
 
