4578737bf27b2810dbb66fa7b0a2a2734f7bbb2a
Drathijin <rsulbara@ucm.es>
2021-04-29 11:15:00 +0200

    Casting entry.path to c string

--- source/Scripting/source/ScriptManager.cpp
+++ source/Scripting/source/ScriptManager.cpp
@@ -71,7 +71,7 @@ namespace PTSD {
 		(*state).require_file("reqSceneConfigurations", "./assets/scripts/Engine/Prefab.lua");
 		for (const auto & entry : fs::directory_iterator( "./assets/scripts/Client/Prefabs"))
 		{
-			(*state).script_file(entry.path());
+			(*state).script_file(entry.path().c_str());
 		}
 
 		(*state).require_file("sampleScene", "./assets/scripts/Client/sampleScene.lua");
