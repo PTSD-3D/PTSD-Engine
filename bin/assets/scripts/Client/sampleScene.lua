10b644d2028552c30d3b59645e0c58bce9011a4b
Pj <pjurad01@ucm.es>
2021-04-30 12:58:13 +0200

    cosa de prueba

--- bin/assets/scripts/Client/sampleScene.lua
+++ bin/assets/scripts/Client/sampleScene.lua
@@ -1,43 +1,41 @@
-local prefabs = reqPrefab
-local sceneConfig = reqSceneConfigurations
-local Scene = {
-	Entities = {
-		Enemy = prefabs.Enemy{
-			Transform = {
-				position = { x = -35.1012802124, y = -7.9060144424, z = 11.6276283264 },
-				rotation = { x = 221.1516898526, y = -152.1657547071, z = 195.3355828656 },
-				scale = { x = 2.098890543, y = 2.098890543, z = 2.0988907814 }
-			}
-		},
-		Enemy_001 = prefabs.Enemy{
-			Transform = {
-				position = { x = 3.6457614899, y = 5.4532294273, z = -2.2724339962 },
-				rotation = { x = -73.3971581794, y = 1.9531190601, z = -45.040504274 },
-				scale = { x = 0.3224791288, y = 0.3224791884, z = 0.3224791288 }
-			}
-		},
-		Enemy_002 = prefabs.Enemy{
-			Transform = {
-				position = { x = 0, y = 0, z = 0 },
-				rotation = { x = -69.7976211661, y = 3.2747051693, z = -42.9677057855 },
-				scale = { x = 1.8980841637, y = 1.8980841637, z = 1.8980841637 }
-			}
-		},
-		Enemy_003 = prefabs.Enemy{
-			Transform = {
-				position = { x = -14.5135574341, y = 8.8633947372, z = -7.8755083084 },
-				rotation = { x = -61.1043681264, y = 5.8524428946, z = -37.7620019775 },
-				scale = { x = 1.0855004787, y = 1.0855004787, z = 1.085500598 }
-			}
-		},
-		Enemy_004 = prefabs.Enemy{
-			Transform = {
-				position = { x = 13.214509964, y = 15.6598205566, z = 16.3075809479 },
-				rotation = { x = -32.491903146, y = -25.2611848528, z = -52.2247567902 },
-				scale = { x = 2.7305061817, y = 2.7305061817, z = 2.7305061817 }
-			}
-		}
-	},
-	SceneConfig = sceneConfig.sampleScene{}
+local prefabs = reqPrefab 
+local sceneConfig = reqSceneConfigurations 
+local Scene={
+ Entities={	Goal=
+        {
+		Transform = {position={x=0.0,y=84.76555633544922,z=-37.48662567138672},rotation={x=0.0,y=0.0,z=0.0},scale={x=0.521728515625,y=0.521728515625,z=0.521728515625}},
+            Components= {
+            }
+        }
+        ,
+	Platform_000=prefabs.Platform({
+		Transform = {position={x=0.0,y=-6.6010847091674805,z=-40.0},rotation={x=0.0,y=0.0,z=0.0},scale={x=10.905817031860352,y=-10.831353187561035,z=1.0}}}),
+	Platform_001=prefabs.Platform({
+		Transform = {position={x=7.747493743896484,y=11.914058685302734,z=-40.0},rotation={x=0.0,y=-18.05662959930421,z=0.0},scale={x=3.8450777530670166,y=-3.6825337409973145,z=3.651704788208008}}}),
+	Platform_002=prefabs.Platform({
+		Transform = {position={x=-4.851972579956055,y=23.379627227783203,z=-40.0},rotation={x=0.0,y=10.730294634208086,z=0.0},scale={x=3.8450777530670166,y=-3.6825337409973145,z=3.651704788208008}}}),
+	Platform_003=prefabs.Platform({
+		Transform = {position={x=7.747493743896484,y=32.83156204223633,z=-40.0},rotation={x=0.0,y=-18.05662959930421,z=0.0},scale={x=3.8450777530670166,y=-3.6825337409973145,z=3.651704788208008}}}),
+	Platform_004=prefabs.Platform({
+		Transform = {position={x=4.768166542053223,y=45.469566345214844,z=-40.0},rotation={x=0.0,y=0.0,z=0.0},scale={x=2.203453540802002,y=4.325438022613525,z=1.0}}}),
+	Platform_005=prefabs.Platform({
+		Transform = {position={x=-5.274786949157715,y=53.66787338256836,z=-40.0},rotation={x=0.0,y=0.0,z=0.0},scale={x=2.203453540802002,y=4.325438022613525,z=1.0}}}),
+	Platform_006=prefabs.Platform({
+		Transform = {position={x=5.200106620788574,y=63.49094009399414,z=-40.0},rotation={x=0.0,y=0.0,z=0.0},scale={x=2.203453540802002,y=4.325438022613525,z=1.0}}}),
+	Platform_007=prefabs.Platform({
+		Transform = {position={x=0.0,y=84.19429016113281,z=-40.0},rotation={x=0.0,y=0.0,z=0.0},scale={x=10.905817031860352,y=-10.831353187561035,z=1.0}}}),
+	Spawn=
+        {
+		Transform = {position={x=0.0,y=-9.152349472045898,z=-37.48662567138672},rotation={x=0.0,y=0.0,z=0.0},scale={x=0.521728515625,y=0.521728515625,z=0.521728515625}},
+            Components= {
+            }
+        }
+        },
+		SceneConfig=
+        {
+        PhysicsConfig={
+            Gravity = 5
+        }
+    }
 }
 return Scene
