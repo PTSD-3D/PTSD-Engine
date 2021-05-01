e81def1f931f340aa11093390846799c47d8b970
Miguel Mur Cortés <mmur@ucm.es>
2021-05-01 12:47:37 +0200

    carga de recursos en lua (aun no funciona nada)

--- bin/assets/scripts/Client/resources.lua
+++ bin/assets/scripts/Client/resources.lua
@@ -6,24 +6,26 @@ local SoundType = {
 }
 local SoundChannel = {
 	Ambiente = 0,
-	Dialogo = 1
+	Dialogo = 1,
+	General = 2
 }
 
 Resources = {
+	PATHS = {
+		Sound = "./assets/sounds/"
+	},
 	Sounds = {
-		_PATH = "./assets/sounds/",
 		Oof = {
-			Path = "oof.mp3", id, tipo=SoundType.Sonido
+			path = "oof.mp3", id, tipo=SoundType.Sonido
 		},
 		PTSD_Anthem = {
-			Path = "PTSD-Anthem.mp3"
-		},
-		Clowning = {
-			Path = "clowning-around.mp3", id, tipo=SoundType.Musica
+			path = "PTSD-Anthem.mp3", id, volume = 0.3
+		},		Clowing = {
+			path = "clowning-around.mp3", id, tipo=SoundType.Musica, volume = 0.3
 		}
 	},
 	Meshes = {
-
+		--si
 	}
 }
 return Resources
--- bin/assets/scripts/Engine/resourceLoader.lua
+++ bin/assets/scripts/Engine/resourceLoader.lua
@@ -1,38 +1,8 @@
 local eng = reqEngine
 local ns = reqNamespace
 
-function MoveSystem:update(dt)
-	for _, entity in pairs(self.targets) do
-		local dir;
-		local vx = entity:get("playerMove").x;
-		local vy = entity:get("playerMove").y;
-		local vz = entity:get("playerMove").z;
-
-		local tr = entity.Transform;
-		if keyPressed(PTSDKeys.A) then
-			dir = vec3:new(-vx, 0, 0)
-			tr:translate(dir)
-		end
-		if keyPressed(PTSDKeys.W) then
-			dir = vec3:new(0, 0, -vz)
-			tr:translate(dir)
-		end
-		if keyPressed(PTSDKeys.S) then
-			dir = vec3:new(0, 0, vz)
-			tr:translate(dir)
-		end
-		if keyPressed(PTSDKeys.D) then
-			dir = vec3:new(vx, 0, 0)
-			tr:translate(dir)
-		end
-		if keyJustPressed(PTSDKeys.Shift) then
-			local material = entity.Mesh:getMaterial()
-			if material == "KirbyMat" then
-				material = "Red"
-			else
-				material="KirbyMat"
-			end
-			entity.Mesh:setMaterial(material);
-		end
+function LoadSounds()
+	for _, sound in pairs(Resources.Sounds) do
+		sound.id = PTSDLoadSound(PATHS.Sound+sound.path,sound.tipo, sound.volume or 1)
 	end
 end
